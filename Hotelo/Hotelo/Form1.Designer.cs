c8d6df0186957e538f5e3eb7dbbb562bc4817adb
Ej-Abao <154227576+Ej-Abao@users.noreply.github.com>
2025-10-24 14:21:30 +0800

    Add files via upload

---
+++ Hotelo/Hotelo/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Hotelo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CustNameTxt = new System.Windows.Forms.TextBox();
+            this.CustEmailTxt = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ReserveBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Mistral", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 95);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hotelo\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.label2.Location = new System.Drawing.Point(23, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hotel Reservation ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.label3.Location = new System.Drawing.Point(129, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.label4.Location = new System.Drawing.Point(129, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.label5.Location = new System.Drawing.Point(84, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Room Type:";
+            // 
+            // CustNameTxt
+            // 
+            this.CustNameTxt.Location = new System.Drawing.Point(196, 141);
+            this.CustNameTxt.Name = "CustNameTxt";
+            this.CustNameTxt.Size = new System.Drawing.Size(161, 22);
+            this.CustNameTxt.TabIndex = 5;
+            // 
+            // CustEmailTxt
+            // 
+            this.CustEmailTxt.Location = new System.Drawing.Point(196, 173);
+            this.CustEmailTxt.Name = "CustEmailTxt";
+            this.CustEmailTxt.Size = new System.Drawing.Size(161, 22);
+            this.CustEmailTxt.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(196, 204);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(161, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // ReserveBtn
+            // 
+            this.ReserveBtn.BackColor = System.Drawing.Color.DarkRed;
+            this.ReserveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ReserveBtn.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.ReserveBtn.Location = new System.Drawing.Point(262, 244);
+            this.ReserveBtn.Name = "ReserveBtn";
+            this.ReserveBtn.Size = new System.Drawing.Size(95, 27);
+            this.ReserveBtn.TabIndex = 8;
+            this.ReserveBtn.Text = "Reserve!";
+            this.ReserveBtn.UseVisualStyleBackColor = false;
+            this.ReserveBtn.Click += new System.EventHandler(this.ReserveBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Info;
+            this.dataGridView1.Location = new System.Drawing.Point(398, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(390, 379);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.label6.Location = new System.Drawing.Point(394, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(173, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Active Reservations:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.label7.Location = new System.Drawing.Point(23, 408);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 27);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Luxury at it\'s finest";
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.BackColor = System.Drawing.Color.DarkRed;
+            this.UpdateBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpdateBtn.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.UpdateBtn.Location = new System.Drawing.Point(196, 295);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(161, 27);
+            this.UpdateBtn.TabIndex = 12;
+            this.UpdateBtn.Text = "Update Reservation";
+            this.UpdateBtn.UseVisualStyleBackColor = false;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.BackColor = System.Drawing.Color.DarkRed;
+            this.DeleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteBtn.ForeColor = System.Drawing.Color.PapayaWhip;
+            this.DeleteBtn.Location = new System.Drawing.Point(196, 328);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(161, 27);
+            this.DeleteBtn.TabIndex = 13;
+            this.DeleteBtn.Text = "Cancel Reservation";
+            this.DeleteBtn.UseVisualStyleBackColor = false;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ReserveBtn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.CustEmailTxt);
+            this.Controls.Add(this.CustNameTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Hotelo Reservation Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox CustNameTxt;
+        private System.Windows.Forms.TextBox CustEmailTxt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button ReserveBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+    }
+}
+
---
+++ Hotelo/Hotelo/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotelo
+{
+    public partial class Form1 : Form
+    {
+        private static HoteloDBEntities HoteloDatabase = new HoteloDBEntities();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void LoadData()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("User ID", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Email", typeof(string));
+            dt.Columns.Add("Room", typeof(string));
+
+            dataGridView1.DataSource = dt;
+
+            var Customer = HoteloDatabase.Customer.ToList();
+
+            foreach (var Customers in Customer)
+            {
+
+                dt.Rows.Add(Customers.CustomerID, Customers.CustomerName, Customers.CustomerEmail, Customers.Room.RoomType);
+            }
+            var Room = HoteloDatabase.Room.ToList();
+                
+            comboBox1.Items.Clear();
+
+            foreach (var Rooms in Room)
+            {
+                comboBox1.Items.Add(Rooms.RoomType);
+
+            }
+        }
+
+        private void ReserveBtn_Click(object sender, EventArgs e)
+        {
+            var Customer = new Customer()
+            {
+                CustomerName = CustNameTxt.Text,
+                CustomerEmail = CustEmailTxt.Text,
+                RoomID = comboBox1.SelectedIndex + 1
+            };
+            
+            HoteloDatabase.Customer.Add(Customer);
+
+            HoteloDatabase.SaveChanges();
+
+            LoadData();
+
+            MessageBox.Show("Room Reserved Succesfully!", "Sucess");
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
